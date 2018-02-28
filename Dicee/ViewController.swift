6beefff4ef60fa636cdfa3308a428cc8f88ca1d5
Shreyas Zagade <shreyaszagade@gmail.com>
2018-02-28 07:21:40 -0500

    Added gitignore and resynced repo

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonClicked(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int(arc4random(6))
+        randomDiceIndex2 = Int(arc4random(6))
+        
+        
+    }
+    
 }
 

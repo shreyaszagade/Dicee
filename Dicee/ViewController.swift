619aa787ae55d974524436e9d4c7f3fbe0104964
Shreyas Zagade <shreyaszagade@gmail.com>
2018-02-28 16:14:40 -0500

    Added Shake gesture

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -43,5 +43,9 @@ class ViewController: UIViewController {
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
 }
 

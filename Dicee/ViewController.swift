3c221d0141cc52707e3a2a1cde016a5dd8d322ec
Shreyas Zagade <shreyaszagade@gmail.com>
2018-02-28 16:07:41 -0500

    Completed random number generations and logic to change image

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,10 +31,16 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonClicked(_ sender: UIButton) {
         
-        randomDiceIndex1 = Int(arc4random(6))
-        randomDiceIndex2 = Int(arc4random(6))
+        updateDiceImages()
         
+    }
+    
+    func updateDiceImages(){
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
         
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
